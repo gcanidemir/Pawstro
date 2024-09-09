0918804abc8e7685205558073a33f7fd163875db
Bugraunluu <bugraunl0607@gmail.com>
2024-09-10 01:20:32 +0300

    inventory

---
+++ Assets/Scripts/OpenInventory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OpenInventory : MonoBehaviour
+{
+    public GameObject inventory;
+    void Start(){
+        inventory.SetActive(false);
+    }
+    // Start is called before the first frame update
+    public void EnableInventory(){
+        inventory.SetActive(!inventory.activeSelf);
+    }
+}
