63382f7bea1636af53bc45df54df342be924bce3
Metkan Canıdemir <121246130+mcanidemir@users.noreply.github.com>
2024-09-09 22:08:09 +0300

    Drill

--- Assets/Scripts/Drill.cs
+++ Assets/Scripts/Drill.cs
@@ -9,6 +9,10 @@ public class Drill : MonoBehaviour
     public bool canFire;
     private float timer;
     public float timeBetweenFire;
+    public LayerMask enemyLayer;
+    public LayerMask meteorLayer;
+    public Transform attackPoint;
+    public float attackRange;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,6 +45,19 @@ public class Drill : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && canFire)
         {
             Debug.Log("týkladýn");
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+            foreach(Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("Enemy hit");
+            }
+            Collider2D[] hitMeteor = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, meteorLayer);
+
+            foreach (Collider2D meteor in hitMeteor)
+            {
+                Debug.Log("Meteor hit");
+            }
+
             canFire = false;
         }
         if (!canFire)
@@ -53,5 +70,9 @@ public class Drill : MonoBehaviour
             }
 
         }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
